f574621920c8ffdf8afd414c8d5f43639d3d9017
Roshantiwary1 <tiwariroshan9999@gmail.com>
2023-01-21 20:12:34 +0530

    added auth and modified sidebar component and used session

--- components/Input.js
+++ components/Input.js
@@ -1,10 +1,15 @@
 import { EmojiHappyIcon, PhotographIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { useSession } from 'next-auth/react'
+import  Image  from 'next/image';
 
 function Input() {
+  const {data:session} = useSession();
+  console.log(session);
   return (
     <div className='flex border-b border-gray-200 p-3 space-x-3 whitespace-nowrap'>
-       <img src="https://images.unsplash.com/photo-1566753323558-f4e0952af115?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mnx8fGVufDB8fHx8&w=1000&q=80" className="h-11 w-11 rounded-full cursor-pointer xl:mr-2 hover:brightness-95" alt="user"/>
+       {session && (<> <Image width="44" height="44" src={session?.user?.image} className="h-11 w-11 rounded-full cursor-pointer xl:mr-2 hover:brightness-95" alt=""/>
+       
        <div className='w-full divide-y divide-gray-200'>
         <div>
             <textarea className='w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700' rows="2" placeholder="'What's Happening?"></textarea>
@@ -16,7 +21,7 @@ function Input() {
             </div>
             <button className='ml-auto bg-blue-500 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-90 disabled:opacity-50'>Tweet</button>
         </div>
-       </div>
+       </div></>)}
     </div>
   )
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,8 @@
 import Image  from 'next/image';
 import SidebarMenuItem from './SidebarMenuItem';
 import { HomeIcon} from "@heroicons/react/solid"
+import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router'
 import {
     BellIcon,
     BookmarkIcon,
@@ -12,6 +14,8 @@ import {
     UserIcon,} from "@heroicons/react/outline";
 
 const Sidebar = () => {
+  const router=useRouter();
+  const {data:session} = useSession()
   return (
     <div className='hidden sm:flex flex-col p-2 lg:items-start fixed h-full xl:ml-24'>
         {/* twitter logo */}
@@ -22,24 +26,27 @@ const Sidebar = () => {
         <div className='mt-4 mb-2.5 '>
         <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        {session && (
+          <><SidebarMenuItem text="Notifications" Icon={BellIcon} />
         <SidebarMenuItem text="Messages" Icon={InboxIcon} />
         <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
         <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
         <SidebarMenuItem text="Profile" Icon={UserIcon} />
-        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} /></>
+        )}
         </div>
         {/* button  */}
-        <button className='text-white rounded-full bg-blue-500 w-56 h-12 font-bold shadow-md hover:brightness-90 text-lg hidden xl:inline '>Tweet</button>
+        {session && <button onClick={signOut} className='text-white rounded-full bg-blue-500 w-56 h-12 font-bold shadow-md hover:brightness-90 text-lg hidden xl:inline '>Logout</button>}
+        {!session && <button onClick={()=>router.push("/auth/signin")} className='text-white rounded-full bg-blue-500 w-56 h-12 font-bold shadow-md hover:brightness-90 text-lg hidden xl:inline '>Signin</button>}
         {/* profile */}
-        <div className='hoverEffect text-gray-700 flex items-center mt-auto '>
-            <img src="https://images.unsplash.com/photo-1566753323558-f4e0952af115?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mnx8fGVufDB8fHx8&w=1000&q=80" className="h-10 w-10 rounded-full xl:mr-2" alt="user"/>
+       {session && ( <div className='hoverEffect text-gray-700 flex items-center mt-auto '>
+            <Image height="40" width="40" src={session.user.image} className="rounded-full xl:mr-2" alt="user"/>
             <div className='leading-5 hidden xl:inline'>
-                <h4 className='font-bold'>Roshan Tiwary</h4>
-                <p className='text-gray-500'>@roshantiwary404</p>
+                <h4 className='font-bold'>{session.user.name}</h4>
+                <p className='text-gray-500'>@{session.user.username}</p>
             </div>
             <DotsHorizontalIcon className='h-5 xl:ml-8 hidden xl:inline'/>
-        </div>
+        </div>)}
     </div>
   )
 }
